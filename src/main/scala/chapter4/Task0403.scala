8517e8bca3cb85ba3a61455714217e86611fa9b2
PisarevskiyRE <daimon--@live.ru>
2024-07-22 15:36:43 +0300

    task 0403

---
+++ src/main/scala/chapter4/Task0403.scala
@@ -0,0 +1,23 @@
+package chapter4
+
+import java.io.File
+
+object Task0403 extends App {
+
+  val in = new java.util.Scanner(new File("D:\\!Work\\Scala\\Projects\\ScalaBook2\\src\\main\\resources\\test.txt"))
+
+  var readText = scala.collection.immutable.Map[String, Int]().withDefaultValue(0)
+
+
+  while (in.hasNext()) {
+
+    val line = in.nextLine().split(" ")
+
+    for(word <- line){
+      readText = readText + (word -> (readText(word) +1 ))
+    }
+  }
+
+  readText.foreach(println)
+
+}
